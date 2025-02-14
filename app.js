889b9a5cb6fd26bf27c395e0128a49991d0e9b4e
MACS1024 <manuelalejandrochucsansores@gmail.com>
2025-02-13 22:51:46 -0600

    Actualizacion del app.js
    Se agrego una nueva funcion para el usuario

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);  
+    elementoHTML.innerHTML = texto;
+    return;  
+}
+
+function limpiarValor(){
+    document.querySelector("#amigo").value = "";
+}
+
+asignarTextoElemento("h1", "Amigo Secreto!");
+asignarTextoElemento("h2", "Escribe el nombre de tus amigos :)");
+
+function agregarAmigo(){
+    let nombreAmigo = document.getElementById("amigo").value;
+
+    if(nombreAmigo == ""){
+        alert("Porfavor, escriba un nombre!!");
+    }else{
+        amigos.push(nombreAmigo);
+        limpiarValor();
+        console.log(amigos);
+    }
+}
