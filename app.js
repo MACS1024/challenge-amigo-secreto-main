974ddbea8133acdc80d2ebd91525a2e58d5d762c
MACS1024 <manuelalejandrochucsansores@gmail.com>
2025-02-18 19:19:10 -0600

    Funcion que sortea un amigo

--- app.js
+++ app.js
@@ -7,35 +7,55 @@ function asignarTextoElemento(elemento, texto) {
     return;  
 }
 
-function limpiarValor(){
-    document.querySelector("#amigo").value = "";
-}
-
-function agregarAmigo(){
-    let nombreAmigo = document.getElementById("amigo").value;
-
-    if(nombreAmigo == ""){
-        alert("Porfavor, escriba un nombre!!");
-    }else{
-        amigos.push(nombreAmigo);
-        limpiarValor();
-    }
-}
-
 function actualizarLista() {
     //1.- Obtener el elemento de la lista
-    let lista = document.getElementById("listaAmigos");
+    let listaAmigos = document.getElementById("listaAmigos");
     //2.- Limpiar la lista existente para evitar duplicados
-    lista.innerHTML = "";
+    listaAmigos.innerHTML = "";
 
     for(let i = 0; i < amigos.leghth; i ++){
         // 4. Crear un nuevo elemento <li> para cada amigo
-        const li = document.createElement('li');
+        const li = document.createElement("li");
         li.textContent = amigos[i]; // Asigna el nombre del amigo
-        li.appendChild(li);
+        listaAmigos.appendChild(li);
     }
+}
+
+function limpiarValor(){
+    document.querySelector("#amigo").value = "";
+    return;
+}
+
+function agregarAmigo(){
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value.trim();
+
+    if(nombreAmigo == ""){
+        alert("Porfavor, escriba un nombre!!");
+        return;
+    }
+    
+    if(amigos.includes(nombreAmigo)){
+        alert(`${nombreAmigo} ya esta en la lista de amigos`)
+        return;
+    }
+
+    amigos.push(nombreAmigo);
+    limpiarValor();
+    actualizarLista();
+}
+
+function sortearAmigo() {
+    let numeroGenerado = Math.ceil(Math.random()*amigos.length);
+
+    console.log(amigos[numeroGenerado]);
+    if (amigos == ""){
+        asignarTextoElemento("h2","No hay amigos para sortear :c");
+    }
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `El amigo sorteado es ${amigos[numeroGenerado]}`;
 }
 
 asignarTextoElemento("h1", "Amigo Secreto!");
 asignarTextoElemento("h2", "Escribe el nombre de tus amigos :)");
-actualizarLista();
